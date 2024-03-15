e9ce8411ff34b43d0f3ccf105216b95c04f6d0ef
Muhammadabdullox Kozimjonov <kozimjonova00@gmail.com>
2024-03-15 19:59:41 +0500

    Abdullox

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=13, validators=[
+        RegexValidator(
+            regex='^[\+]9{2}8{1}[0-9]{9}$',
+            message='Invalide phone number',
+            code='Invalid number'
+        )
+    ])
+    img = models.ImageField(upload_to='profile_img', null=True, blank=True)
+
+    class Meta(AbstractUser.Meta):
+        swappable= 'AUTH_USER_MODEL'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
