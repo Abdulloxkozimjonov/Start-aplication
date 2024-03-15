23ac7417890cb009685ba3b7b64770863db5a6eb
Muhammadabdullox Kozimjonov <kozimjonova00@gmail.com>
2024-03-15 20:09:39 +0500

    Abdullox

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from main.models import User
 
-# Create your views here.
+
+def login_user(request):
+    return render(request,'login.html',)
+
+
+def register_user(request):
+    return render(request, 'reg.html')
