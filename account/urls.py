46ef6e3b870dd8a84e0ba807ae97259c2958b240
Muhammadabdullox Kozimjonov <kozimjonova00@gmail.com>
2024-03-17 09:55:06 +0500

    Abdullox

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from .views import *
 
-urlpatterns = []
+urlpatterns = [
+    path('create-user/', create_user, name='create_user'),
+    path('login-user/', login_user, name='login_user'),
+    path('update-user/', update_user, name='update_user'),
+    path('log-out-user/', logout_user, name='logout_user'),
+]
